0ea6d5358290ff7e03d0926ac0a6887ccbce3879
Destin Meza <60479193+DestinMeza@users.noreply.github.com>
2024-11-19 19:17:15 -0600

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "Box2D"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.c",
+		
+		"include/**.h"
+	}
+
+	includedirs
+	{
+    "src"
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++latest"
+		staticruntime "off"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
